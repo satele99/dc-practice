9f34fea1c76a9a96783c8de9d8fb55ebf034e390
Amir Ali <satele.works@gmail.com>
2022-10-29 12:30:52 -0400

    added object-practice.js file

---
+++ programming-fundamentals/object-practice.js
@@ -0,0 +1,24 @@
+let customer = {
+        firstName : "Amir",
+        lastName: "Ali", 
+        phone: "555-555",
+        email: "string@email.com",
+        address: "351 Fake address",
+    
+};
+
+let order = {
+    orderNumber: 123,
+    orderDate: '10-29-2022',
+    totalQty: 3,
+    totalAmount: 3.00,
+};
+
+let itemRelation = {
+    itemNumber: 222,
+    itemName: 'Toy',
+    quantity: 10,
+    price: 3.00,
+};
+
+console.log(customer.firstName);
